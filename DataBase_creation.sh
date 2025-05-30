b074b5380a69f9ccb911c8988abbcb3341a43617
SaraNourAllah <saranourallah@gmail.com>
2025-05-30 22:44:30 +0300

    Add files via upload
    DataBase_creation file added.

---
+++ DataBase_creation.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+DB_PATH="./DB"
+mkdir -p "$DB_PATH"
+
+function main_menu() {
+    echo "Welcome to Bash DBMS"
+    PS3="Main Menu - Select an option: "
+    select option in "Create Database" "List Databases" "Connect to Database" "Drop Database" "Exit"; do
+        case $REPLY in
+            1) create_database ;;
+            2) list_databases ;;
+            3) connect_database ;;
+            4) drop_database ;;
+            5) echo "Exit"; break ;;
+            *) echo "Invalid option, Try again." ;;
+        esac
+    done
+}
+
+main_menu
